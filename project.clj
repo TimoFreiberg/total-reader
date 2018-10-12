acd1bce43fb8b656f6312936c13d97e8604fde73
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-10-12 15:07:33 +0200

    Version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject total-reader "1.0.0-SNAPSHOT"
+(defproject total-reader "1.0.0"
   :description "A clojure reader that can resolve namespace aliased keywords."
   :url "http://github.com/TimoFreiberg/total-reader"
   :license {:name "Eclipse Public License"
